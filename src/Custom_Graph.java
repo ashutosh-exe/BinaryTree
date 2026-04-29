9f3ab24eb612c2b6e10502477bf02d684b67ca6a
ashutosh-exe <ashutoshsingh200427@gmail.com>
2026-04-29 23:18:50 +0530

    Basic Graph with BFS traversal.

--- src/Custom_Graph.java
+++ src/Custom_Graph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class Custom_Graph {
     private int V;
@@ -24,11 +26,36 @@ public class Custom_Graph {
             System.out.println();
         }
     }
+    public void BFS(int startNode){
+        boolean[] visited = new boolean[V];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startNode] = true;
+        queue.add(startNode);
+
+        System.out.print("BFS traversal starting from node "+ startNode+":");
+
+        while(!queue.isEmpty()){
+            int currentNode = queue.poll();
+            System.out.print(currentNode+" ");
+
+            for(int neighbor:adj[currentNode]){
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Custom_Graph g = new Custom_Graph(5);
         g.addEdge(0,1);
         g.addEdge(0,4);
         g.addEdge(1,2);
         g.printGraph();
+
+        g.BFS(0);
     }
 }
